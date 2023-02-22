b6d92bc2d8d6df5b8815769adb1dc3b55cbb194c
141700 <box41700@gmail.com>
2023-02-22 17:40:44 +0300

    Ver 1.4: Added old games cleaning and app resetting.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 This is app for handling playroom for The Resistance table game.
 This app is just for Java coding practice.
 
-See https://resistance-table-game.herokuapp.com/
+Play at https://resistancegame.alexwolf.ru (Russian)
--- src/main/java/resistanceGame/controller/GameplayController.java
+++ src/main/java/resistanceGame/controller/GameplayController.java
@@ -1,13 +1,18 @@
 package resistanceGame.controller;
 
-import resistanceGame.form.MainForm;
-import resistanceGame.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import resistanceGame.ResistanceGameApplication;
+import resistanceGame.form.MainForm;
+import resistanceGame.service.GameService;
 import resistanceGame.service.RequestService;
+
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Controller
@@ -41,6 +46,13 @@ public class GameplayController {
     public String handleSessionSubmit(MainForm form, Model model) {
         model.addAttribute("gameplay", requestService.handleIncomeRequest(form));
         return "match";
+    }
+
+    @GetMapping("/restartapp")
+    public String handleRestartRequest(MainForm form, Model model) {
+        ResistanceGameApplication.restart();
+        model.addAttribute("opened", Collections.EMPTY_MAP);
+        return "home";
     }
 }
 
